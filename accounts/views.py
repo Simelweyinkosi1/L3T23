d28402cda6f624f6f27435d41f227108203b8ea6
Simelweyinkosi <sibindisme@gmail.com>
2023-08-16 10:40:34 +0200

    accounts docs

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from .forms import SignUpForm
 
 
 def signup(request):
+    '''Signs a user in'''
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
